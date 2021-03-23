008c0b633e6a8bc4768223a1c4f6cf11475ea43f
Adarsh Chavali <adarsh@junelife.com>
2021-03-23 18:15:26 +1300

    Workaround for SPM bug

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ import PackageDescription
 let package = Package(
         name: "Starscream",
         products: [
-            .library(name: "Starscream", targets: ["Starscream"])
+            .library(name: "Starscream", type: .dynamic, targets: ["Starscream"])
         ],
         dependencies: [
           .package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0")
